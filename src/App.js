85cfa166270751015b425adfac12a64c2e938f60
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2026-01-25 20:13:02 +0530

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Fragment } from 'react/jsx-runtime';
 import './App.css';
 import { ProductCard } from './components/ProductCard';
 import { ProductList } from './components/ProductList';
@@ -6,21 +7,24 @@ function App() {
   const products = [{
     imageSrc: "images/iphone.png",
     title: "iphone 15 pro",
-    specifications: ["A17 pro chip with 6 core-GPU", "3x or 5x Telephoto camera", "upto 29 hours video playback"],
+    specification: ["A17 pro chip with 6 core-GPU", "3x or 5x Telephoto camera", "upto 29 hours video playback"],
+    stockCount: 10,
     price: "999",
 
   },
   {
     imageSrc: "images/airpods.png",
     title: "Airpods pro 2",
-    specifications: ["Noise cancellation", "Dust, sweat, and water resistant", "upto 6 hours listening"],
+    specification: ["Noise cancellation", "Dust, sweat, and water resistant", "upto 6 hours listening"],
+    stockCount: 0,
     price: "249",
 
   },
   {
     imageSrc: "images/apple-watch.png",
     title: "Apple watch 9",
-    specifications: ["45mm or 41mm case size", "Always-on retina display", "upto 18 hours normal use"],
+    specification: ["45mm or 41mm case size", "Always-on retina display", "upto 18 hours normal use"],
+    stockCount: 6,
     price: "399",
 
   },
@@ -29,14 +33,22 @@ function App() {
   function handlePurchase(product) {
     alert(`You have clicked on ${product.title} which cost $${product.price}`)
   }
-  
+
   return (
     <div className="App">
       <ProductList>
-        <ProductCard width="96px" height="96px" background="darkolivegreen" product={products[0]} onPurchas={handlePurchase} />
-        <ProductCard width="64px" height="64px" product={products[1]} onPurchas={handlePurchase}/>
-        <ProductCard width="128px" height="128px" background="peru" product={products[2]} onPurchas={handlePurchase}/>
+        {products.map((product) => (
+          <ProductCard key={product.title} product={product} onPurchase={handlePurchase} />
+        ))}
       </ProductList>
+
+      <h2>Product which cost upto $500</h2>
+      {products.filter(({ price }) => price < 500).map(({ title, price }) => (
+        <Fragment key={title}>
+          <hr style={{borderColor:"slategray"}}/>
+          <p>{title} cost ${price}</p>
+        </Fragment>
+      ))}
     </div>
   );
 }
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,19 +1,31 @@
-export function ProductCard({ product, background ="slategray", onPurchase, ...restProps}) {
-    
+export function ProductCard({ product, background = "slategray", onPurchase, }) {
+
     return (
         <article style={{ background, width: "100%", border: "1px solid white", borderRadius: "8px", padding: "16px", textAlign: "center" }}>
             <h2>{product.title}</h2>
             <img src={product.imageSrc}
                 alt={product.title}
-                {...restProps}
+                width={128}
+                height={128}
             />
             <p>Specifications</p>
             <ul style={{ listStyle: "none", padding: 0 }}>
-                <li>{product.specifications[0]}</li>
-                <li>{product.specifications[1]}</li>
-                <li>{product.specifications[2]}</li>
+                {product.specification.map((spec, index) => (
+                    <li key={index}> {spec}</li>
+                ))}
             </ul>
-            <button onClick={() => onPurchase(product)}>Buy (From ${product.price})</button>
+            <Status stockCount={product.stockCount} />
+            {product.stockCount > 0 && (<button onClick={() => onPurchase(product)}>Buy (From ${product.price})</button>)}
         </article>
     )
+}
+
+function Status({ stockCount }) {
+    const notAvailableTemplate = (
+        <p style={{ fontSize: "14px", color: "lightsalmon" }}>Not available</p>
+    );
+    const availableTemplate = (<p style={{ fontSize: "14px", color: "lightgreen" }}>{stockCount} items available</p>
+    );
+
+    return stockCount === 0 ? notAvailableTemplate : availableTemplate;
 }
